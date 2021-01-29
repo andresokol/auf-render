15a1a5bfe8b9043785da3df519c935add788d711
Andrey Sokolov <sokolov-as-i@yandex.ru>
2021-01-29 23:55:49 +0300

    initial refactoring

---
+++ src/application.hpp
@@ -0,0 +1,123 @@
+#pragma once
+
+#include <optional>
+#include <vector>
+
+#include <vulkan/vulkan.h>
+
+#define GLFW_INCLUDE_VULKAN
+
+#include <GLFW/glfw3.h>
+
+struct QueueFamilyIndices {
+  std::optional<uint32_t> graphicsFamily{};
+  std::optional<uint32_t> presentFamily{};
+
+  bool isComplete() { return graphicsFamily.has_value() && presentFamily.has_value(); }
+};
+
+struct SwapChainSupportDetails {
+  VkSurfaceCapabilitiesKHR capabilities;
+  std::vector<VkSurfaceFormatKHR> formats{};
+  std::vector<VkPresentModeKHR> presentModes{};
+};
+
+namespace aufr {
+
+class Application {
+ public:
+  void run() {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
+
+ private:
+  GLFWwindow *window = nullptr;
+  VkInstance instance;
+  VkDebugUtilsMessengerEXT debugMessenger;
+  VkSurfaceKHR surface;
+  VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+  VkDevice device;
+  VkQueue graphicsQueue;
+  VkQueue presentQueue;
+  VkSwapchainKHR swapChain;
+  std::vector<VkImage> swapChainImages;
+  VkFormat swapChainImageFormat;
+  VkExtent2D swapChainExtent;
+  std::vector<VkImageView> swapChainImageViews;
+  VkRenderPass renderPass;
+  VkPipelineLayout pipelineLayout;
+  VkPipeline graphicsPipeline;
+  std::vector<VkFramebuffer> swapChainFramebuffers;
+  VkCommandPool commandPool;
+  std::vector<VkCommandBuffer> commandBuffers;
+  VkSemaphore imageAvailableSemaphore;
+  VkSemaphore renderFinishedSemaphore;
+
+  void initWindow();
+
+  void initVulkan();
+
+  void mainLoop();
+
+  void createInstance();
+
+  void cleanup();
+
+  void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT &createInfo);
+
+  void setupDebugMessenger();
+
+  std::vector<const char *> getRequiredExtensions();
+
+  bool checkValidationLayerSupport();
+
+  static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+                                                      VkDebugUtilsMessageTypeFlagsEXT messageType,
+                                                      const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData,
+                                                      void *pUserData);
+
+  void pickPhysicalDevice();
+
+  bool isDeviceSuitable(VkPhysicalDevice device);
+
+  bool checkDeviceExtensionSupport(VkPhysicalDevice device);
+
+  void createLogicalDevice();
+
+  void createSurface();
+
+  QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+
+  SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
+
+  VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR> &availableFormats);
+
+  VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR> &availablePresentModes);
+
+  VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR &capabilities);
+
+  void createSwapChain();
+
+  void createImageViews();
+
+  void createRenderPass();
+
+  VkShaderModule createShaderModule(const std::vector<char> &code);
+
+  void createGraphicsPipeline();
+
+  void createFramebuffers();
+
+  void createCommandPool();
+
+  void createCommandBuffers();
+
+  void createSemaphores();
+
+  void drawFrame();
+};
+
+}  // namespace aufr
